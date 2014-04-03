48be2e6f50ae7462b78e17e5ff136b77f567fb6c
Kent C. Dodds <kent@doddsfamily.us>
2014-04-03 15:02:01 -0600

    Bug fixes, and ellipsis

--- bs.common.directives/bsPostStream.js
+++ bs.common.directives/bsPostStream.js
@@ -5,21 +5,30 @@ angular.module('bs.common.directives').directive('bsPostStream', function(_) {
     scope: {
       postsAndShares: '=bsPostStream'
     },
-    link: function(scope) {
-      if (_.isEmpty(scope.postsAndShares)) {
-        return;
-      }
-      scope.posts = scope.postsAndShares.posts;
-      _.each(scope.posts, function(post) {
-        post.sortDate = post.created;
-      });
-      _.each(scope.postsAndShares.shares, function(share) {
-        var post = share.getPost();
-        post.sortDate = share.created;
-        post.share = share;
-        if (!_.contains(scope.posts, post)) {
-          scope.posts.push(post);
+    link: function(scope, el, attrs) {
+      scope.$watch('postsAndShares.posts.length + postsAndShares.shares.length', function() {
+        if (_.isEmpty(scope.postsAndShares)) {
+          return;
         }
+        scope.bsPosts = [];
+        _.each(scope.postsAndShares.posts, function(post) {
+          if (!attrs.authorToShow || attrs.authorToShow === post.owner) {
+            scope.bsPosts.push({
+              post: post,
+              sortDate: post.created
+            });
+          }
+        });
+        _.each(scope.postsAndShares.shares, function(share) {
+          var post = share.getPost();
+          if (!attrs.authorToShow || attrs.authorToShow === post.owner) {
+            scope.bsPosts.push({
+              post: post,
+              share: share,
+              sortDate: share.created
+            });
+          }
+        });
       });
     }
   };
